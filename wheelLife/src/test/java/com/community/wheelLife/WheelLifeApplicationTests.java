4f59df619822854ff8470185bbe89db24181ce59
seongmiin00 <cor2580@gmail.com>
2024-01-18 13:31:11 +0900

    프로젝트 업로드

---
+++ wheelLife/src/test/java/com/community/wheelLife/WheelLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.wheelLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
